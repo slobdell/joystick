79ec6654c4e42975f7ee586e10fbe4caa92e462c
Scott Lobdell <slobdell@uber.com>
2015-09-13 22:53:29 -0700

    joystick size

--- js/joystick_view.js
+++ js/joystick_view.js
@@ -181,8 +181,8 @@ JoystickView = Backbone.View.extend({
         var originalWidth = 89;
         var originalHeight = 89;
 
-        var spriteWidth = 50;
-        var spriteHeight = 50;
+        var spriteWidth = 150;
+        var spriteHeight = 150;
         var pixelsLeft = 0; //ofset for sprite on img
         var pixelsTop = 0; //offset for sprite on img
         var coords = this._cartesianToCanvas(this.x, this.y);
